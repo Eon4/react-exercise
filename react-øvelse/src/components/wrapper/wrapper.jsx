ca69e5b545b3ceefddea8140aaa4d8f43b49176f
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-25 13:32:04 +0200

    new files and wrapper update

--- react-øvelse/src/components/wrapper/wrapper.jsx
+++ react-øvelse/src/components/wrapper/wrapper.jsx
@@ -1,10 +1,18 @@
-import style from './Wrapper.module.scss'
+import style from './wrapper.module.scss'
 
 export function Wrapper({children, title}) {
+
+    const randomColor = () => {
+        let r = Math.floor(Math.random(0) * 255)
+        let g = Math.floor(Math.random(0) * 255)
+        let b = Math.floor(Math.random(0) * 255)
+        let randomString = `rgb(${r},${g},${b})`
+        return randomString
+    }
     return (
         <>
-            <h2 style={{textAlign: "center"}}>{title}</h2>
-            <section className={style.wrapper}>
+            <section className={style.wrapper} style={{borderColor: randomColor()}}>
+            <h2 className={style.heading} style={{textAlign: "center"}}>{title}</h2>
                 {children}
             </section>
         </>
