22dabc99c0316c30a6601ba85cd0cf20aae92ed6
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-25 09:22:24 +0200

    update

--- react-øvelse/src/components/exercise7/exercise7.jsx
+++ react-øvelse/src/components/exercise7/exercise7.jsx
@@ -1,24 +1,28 @@
-// import { useState } from "react;"
+import { useState } from "react";
 import { Wrapper } from "../wrapper/wrapper";
 import style from './exercise7.module.scss'
 
 export function Exercise7() {
 
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    function alertUser() {
+        alert (`Hello ${firstName}  ${lastName}`)
+    }
+
+    console.log("firstname: ", firstName)
+    console.log("lastname: ", lastName)
 
     return (
-
-    
-
-   <Wrapper title ="Exercise 7">
+    <Wrapper title ="Exercise 7">
     <form className={StylePropertyMap.formStyle}>
         <div>
-            <input name="firstname" placeholder="skriv dit navn her" />
+            <input onChange={(event) => setFirstName(event.target.value)}  placeholder="skriv dit navn her" />
+            <input onChange={(event) => setLastName(event.target.value)}  placeholder="lastnavn her" />
+            <button onClick={ alertUser} type="button">Greet me</button>
         </div>
-
     </form>
-
    </Wrapper>
 
 
