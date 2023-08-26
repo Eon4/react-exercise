b0b8e47271ef1ce4fe54b0583e952d1676fae968
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-26 21:11:21 +0200

    upd

--- react-øvelse/src/components/exercise3/alertbutton.jsx
+++ react-øvelse/src/components/exercise3/alertbutton.jsx
@@ -3,6 +3,6 @@ import style from "../exercise3/exercise3.module.scss";
 export function AlertButton(props){
 
     return (
-        <button className={style.buttonStyle} onClick={() => alert('You clicked on ' + props.title)}>{props.title}</button>
+        <button className={style.buttonStyled} onClick={() => alert('You clicked on ' + props.title)}>{props.title}</button>
     )
 }
